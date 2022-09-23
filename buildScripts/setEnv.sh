5bf711da9118a708560cd4d9617c06ad32771c6d
WmDevOps Training <wmdevops@training.nowhere>
2022-09-23 06:36:33 +0000

    setEnv.sh

--- buildScripts/setEnv.sh
+++ buildScripts/setEnv.sh
@@ -4,7 +4,6 @@ export MY_AZ_ACR_URL="azdevopsdhl5acr.azurecr.io"
 export AZ_BASE_IMAGE_REPO="msr-1011-lean-original-recipe"
 export AZ_BASE_IMAGE_FIXES_TAG="Fixes_22-09-20"
 export
-AZ_BASE_IMAGE_TAG="${MY_AZ_ACR_URL}/${AZ_BASE_IMAGE_REPO}:${AZ_BASE_IMA
-GE_FIXES_TAG}"
+AZ_BASE_IMAGE_TAG="${MY_AZ_ACR_URL}/${AZ_BASE_IMAGE_REPO}:${AZ_BASE_IMAGE_FIXES_TAG}"
 export AZ_ACR_REPO_NAME="my-news-service-01"
 export OUR_SERVICE_TAG_BASE="${MY_AZ_ACR_URL}/${AZ_ACR_REPO_NAME}"

4f782682d0bc4a2cfcfbbfc91ce6ac283ad60ec5
WmDevOps Training <wmdevops@training.nowhere>
2022-09-23 06:49:11 +0000

    update prepareAgentMachine.sh

--- buildScripts/02.prepareAgentMachine.sh
+++ buildScripts/02.prepareAgentMachine.sh
@@ -7,13 +7,8 @@ echo "Installing prerequisites..."
 sudo apt-get install -y ca-certificates curl gnupg2 fuse-overlayfs
 . /etc/os-release
 echo "Installing buildah for OS release ${VERSION_ID}..."
-echo "deb
-http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/
-stable/xUbuntu_${VERSION_ID}/ /" | sudo tee
-/etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-curl -fsL
-"https://download.opensuse.org/repositories/devel:kubic:libcontainers:s
-table/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add - &&
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -fsL "https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key" | sudo apt-key add - &&
 sudo apt-get -qq -y update
 sudo apt-get -qq -y install buildah
 echo "Machine prepared"
